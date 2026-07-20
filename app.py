67cf3e462f5adc1514e25942a054477aa88afa62
Akshay Raj <akshayraj2545@gmail.com>
2026-07-20 13:32:53 +0530

    MSG91 direct OTP

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ import random
 import requests
 import os
 from werkzeug.utils import secure_filename
-from dotenv import load_dotenv
 # from voice_ai import speech_to_text
-load_dotenv()
 
 app = Flask(__name__)
 
@@ -993,49 +991,6 @@ def ask_ai_route():
 
     return jsonify({
         "answer": answer
-    })
-
-@app.route("/verify-msg91", methods=["POST"])
-def verify_msg91():
-
-    data = request.get_json()
-
-    access_token = data.get("accessToken")
-
-    authkey = os.getenv("MSG91_AUTH_KEY")
-
-    url = "https://control.msg91.com/api/v5/widget/verifyAccessToken"
-
-    payload = {
-        "authkey": authkey,
-        "access-token": access_token
-    }
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    response = requests.post(
-        url,
-        json=payload,
-        headers=headers
-    )
-
-    result = response.json()
-
-    print(result)
-
-    if result.get("type") == "success":
-
-        session["buyer_logged_in"] = True
-        session["role"] = "buyer"
-
-        return jsonify({
-            "success": True
-        })
-
-    return jsonify({
-        "success": False
     })
 
  #@app.route("/voice_upload", methods=["POST"])
